9deb1729bb8b69bdaee5b58bb18b26e4b914c987
tyeth <tyethgundry@googlemail.com>
2023-11-01 13:11:25 +0000

    WIP: Todo's and Last fortnight for diff

--- debugarduino.py
+++ debugarduino.py
@@ -26,3 +26,21 @@ timestamp = time.strftime("%Y-%m-%d_%H%M%S")
 
 arduino_libraries.main(verbosity=1, output_file=f"{timestamp}.md",ci=1)
 print("done")
+
+
+## TODO:
+# Add CI checking after PR is created. Maybe output PRs to json, recheck file on load.
+# Start with single PR + action review, once fail/pass then add more PRs. Eventually parallelize.
+# Failures should be printed with URLS to runs, so that we can see what failed.
+# User should be asked at run to archive old runs (rename file) or recheck.
+# Also asked if individual PRs should be checked or all automatically.
+# If archiving, then ask if delete/close old draft version bump PRs (linked in json file), assume yes.
+# Also delete branch for PRs that were merged or closed.
+# If rechecking, then ask if delete/close failing-actions draft version bump PRs (newly linked in json file), assume no.
+# If actions pass then merge and delete branch for version bump PR.
+# If actions fail then delete/close version bump PRs and branch (if user agreed, else just log and save in json).
+# If user wants to recheck individual PRs, then check PR (action results) one by one and ask each time if failure then delete PR (or if pass then merge PR question), else recheck all.
+
+
+## CI check flag should change to be about Readme / board assets / CI only changes, log accordingly.
+# Current Issues for Repos and PRs could be shown, ideally with days Old and last updated/commented.
